b992cca6b938f4e19d4f53060d26abf6fd65b682
Prateek Kumar <89418989+prateekkumaroriginal@users.noreply.github.com>
2023-06-16 18:22:33 +0530

    Update views.py

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
-from django.http import Http404
 from django.db.models import Avg
 
 # Create your views here.
@@ -9,7 +8,7 @@ def index(request):
     all_books = Book.objects.all()
     all_books = Book.objects.all().order_by("title") # gets sorted by title
     all_books = Book.objects.all().order_by("-title") # gets sorted by title in decreasing order
-    nr_total_books = len(all_books)
+    nr_total_books = all_books.count()
     avg_rating = all_books.aggregate(Avg("rating"))
     return render(request, "book_outlet/index.html", {
         "all_books": all_books,
@@ -18,10 +17,6 @@ def index(request):
     })
 
 def book_detail(request, slug):
-    # try:
-    #     book = Book.objects.get(pk=id)
-    # except:
-    #     raise Http404()
     book = get_object_or_404(Book, slug=slug)
     return render(request, "book_outlet/book_detail.html", {
         "title": book.title,
