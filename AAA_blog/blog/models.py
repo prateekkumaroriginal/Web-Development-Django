b986d61d2f561e5d8f17bd02eae49be2ae2d472c
Prateek Kumar <89418989+prateekkumaroriginal@users.noreply.github.com>
2023-06-17 05:28:57 +0530

    Added 3 new models
    Added new models: "Post", "Tag", "Author"

--- AAA_blog/blog/models.py
+++ AAA_blog/blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.caption
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(default="", unique=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post-detail", args=[self.slug])
