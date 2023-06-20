9bba0c1f9560267ba5351aa1fcbde7c3ee19bdbf
Prateek Kumar <89418989+prateekkumaroriginal@users.noreply.github.com>
2023-06-21 02:41:53 +0530

    Update urls.py

--- AAA_blog/blog/urls.py
+++ AAA_blog/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('posts/', views.posts, name="posts"),
-    path('posts/<slug:slug>', views.post_detail, name="post-detail"),
+    path('', views.IndexView.as_view(), name="index"),
+    path('posts/', views.AllPostsView.as_view(), name="posts"),
+    path('posts/<slug:slug>', views.PostDetailView.as_view(), name="post-detail"),
 ]
