948f19c8205c3026a34aae415ed810f7fe3bf97b
Prateek Kumar <89418989+prateekkumaroriginal@users.noreply.github.com>
2023-06-21 02:42:19 +0530

    Update models.py

--- AAA_blog/blog/models.py
+++ AAA_blog/blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.CharField(max_length=200)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(default="", unique=True)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
@@ -36,3 +36,10 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("post-detail", args=[self.slug])
+
+class Comment(models.Model):
+    username = models.CharField(max_length=50)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    related_post = models.ForeignKey(Post, on_delete=models.UniqueConstraint, related_name="comments")
+    
