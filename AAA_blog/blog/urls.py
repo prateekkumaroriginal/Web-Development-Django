1cd9ef254cffa7ade68dd546e611c1ae49139eaf
Prateek Kumar <89418989+prateekkumaroriginal@users.noreply.github.com>
2023-06-21 15:21:40 +0530

    Update urls.py

--- AAA_blog/blog/urls.py
+++ AAA_blog/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('posts/', views.AllPostsView.as_view(), name="posts"),
     path('posts/<slug:slug>', views.PostDetailView.as_view(), name="post-detail"),
+    path('read-later', views.ReadLaterView.as_view(), name="read-later"),
 ]
